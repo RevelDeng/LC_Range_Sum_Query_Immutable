a2052e2e6af1e816ae142a280db1ae0c1771f672
RevelDeng <84701226+RevelDeng@users.noreply.github.com>
2022-03-30 18:49:40 -0400

    LC Range Sum Query - Immutable

---
+++ range_sum_query.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ */
+var NumArray = function(nums) {
+    this.NumArray = [...nums]
+};
+
+/** 
+ * @param {number} left 
+ * @param {number} right
+ * @return {number}
+ */
+NumArray.prototype.sumRange = function(left, right) {
+    var sum = 0
+    while (left <= right) {
+        sum += this.NumArray[left]
+        left++
+    }
+    return sum
+};
+
+/** 
+ * Your NumArray object will be instantiated and called as such:
+ * var obj = new NumArray(nums)
+ * var param_1 = obj.sumRange(left,right)
+ */
